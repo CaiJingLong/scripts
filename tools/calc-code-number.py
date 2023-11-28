24c5b231414bf096d8e3b422654ad0a0f18259a7
Caijinglong <cjl_spy@163.com>
2023-11-28 10:03:57 +0800

    Update calc-code-number.py

--- tools/calc-code-number.py
+++ tools/calc-code-number.py
@@ -1,29 +1,55 @@
+#!/usr/bin/env python3
+
 import os
 
-script_path = os.path.dirname(os.path.abspath(__file__))
+import argparse
 
-ext = '.kt,.java'
+parser = argparse.ArgumentParser(description="Calculate code number.")
 
-ext_list = ext.split(',')
+parser.add_argument(
+    "-d",
+    "--dir",
+    type=str,
+    default=".",
+    help="The directory to calculate code number.",
+)
 
-for i in range(len(ext_list)):
-    ext_list[i] = ext_list[i].strip()
+parser.add_argument(
+    "-e",
+    "--ext",
+    type=str,
+    default=".dart",
+    help="The file extension to calculate code number.",
+)
+
+parser.add_argument(
+    "-c",
+    "--check-empty-line",
+    action="store_true",
+    help="Check empty line or not.",
+)
+
+args = parser.parse_args()
 
 
-def get_line_number(file, check_empty_line=False):
+def get_line_number(file):
     count = 0
-    with open(file, 'r', encoding='utf-8') as f:
+    empty = 0
+    with open(file, "r", encoding="utf-8") as f:
         for line in f:
-            if check_empty_line:
-                if line.strip() != '':
-                    count += 1
-            else:
-                count += 1
-    return count
+            count += 1
+            if line.strip() == "":
+                empty += 1
+    return count, empty
 
 
-if __name__ == '__main__':
-    dir_path = f'{script_path}/../src'
+if __name__ == "__main__":
+    dir_path = args.dir
+    ext_list = args.ext.split(",")
+    check_empty_line = args.check_empty_line
+    for i in range(len(ext_list)):
+        ext_list[i] = ext_list[i].strip()
+
     dirs = os.walk(dir_path)
 
     total_file_count = 0
@@ -34,13 +60,13 @@ if __name__ == '__main__':
         for file in dir[2]:
             if file.endswith(tuple(ext_list)):
                 total_file_count += 1
-                file_path = f'{dir[0]}/{file}'
-                line_count = get_line_number(file_path)
-                empty_line_count = get_line_number(file_path, True)
-                lines += line_count
-                empty_lines += empty_line_count
-                print(f'{file_path} | {line_count} | {empty_line_count}')
+                file_path = f"{dir[0]}/{file}"
+                line_count, empty_count = get_line_number(file_path)
 
-    print(f'file count: {total_file_count}')
-    print(f'line count: {lines}')
-    print(f'empty line count: {empty_lines}')
+                lines += line_count
+                empty_lines += empty_count
+
+    print(f"file count: {total_file_count}")
+    print(f"line count: {lines}")
+    print(f"empty line count: {empty_lines}")
+    print(f"non-empty line count: {lines - empty_lines}")
